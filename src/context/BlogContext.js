165f1af3368d7100ee45910e40284cead1e671b0
mamuncseru <mamun.cseian@gmail.com>
2021-05-24 09:30:59 +0600

    Everything is complete fro now

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,21 +2,56 @@ import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case 'edit_blogpost':
+      return state.map((blogPost) => {
+          return blogPost.id === action.payload.id
+            ? action.payload
+            : blogPost;
+      });
+    case 'delete_blogpost':
+      return state.filter((blogPost) => blogPost.id !== action.payload );
     case 'add_blogpost':
-      return [...state, { title: `Blog Post #${state.length + 1}` }];
+      return [...state, { 
+          id: Math.floor(Math.random() * 99999), 
+          title: action.payload.title,
+          content: action.payload.content
+        }
+      ];
     default:
       return state;
   }
 };
 
+
 const addBlogPost = dispatch => {
-  return () => {
-    dispatch({ type: 'add_blogpost' });
+  return (title, content, callback) => {
+    dispatch({ type: 'add_blogpost', payload: { title: title, content: content} });
+    if (callback) {
+      callback();
+    }
   };
 };
 
+const editBlogPost = dispatch => {
+  return (id, title, content, callback) => {
+    dispatch({ 
+      type: 'edit_blogpost', 
+      payload: { id: id, title: title, content: content }
+    });
+    if (callback) {
+      callback();
+    }
+  };
+};
+
+const deleteBlogPost = dispatch => {
+  return (id) => {
+    dispatch({ type: 'delete_blogpost', payload: id })
+  };
+}
+
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost },
-  []
+  { addBlogPost, deleteBlogPost, editBlogPost },
+  [{ title: 'TEST POST', content: 'TEST', id:1 }]
 );
